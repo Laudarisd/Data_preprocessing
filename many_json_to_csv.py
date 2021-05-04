7e991111346772ce9242fa6b2f434b008ddee32c
Laudari Sudip <53330430+Laudarisd@users.noreply.github.com>
2021-05-04 10:18:11 +0900

    Update many_json_to_csv.py

--- many_json_to_csv.py
+++ many_json_to_csv.py
@@ -1,15 +1,39 @@
+#Parse a single json file
+
+
+import json
+
+# read file
+with open('./annotations/_-0aygxELCt_AvFtXT-iOA.json', 'r') as myfile:
+    data=myfile.read()
+
+# parse file
+obj = json.loads(data)
+
+# show values
+#print("usd: " + str(obj['usd']))
+#print("eur: " + str(obj['eur']))
+#print("gbp: " + str(obj['gbp']))
+print(obj)
+
+###############################################################################
+#Convert all json to a single csv file. 
+# Csv will include file name, width, height, key, label, xmin, ymin, x max, ymax
+
+
 import glob
 import json 
 import csv
 import os
 
-dir = "./"
+dir = "./annotations/"
+save_dir = "./"
 
-with open(os.path.join(dir, 'test.csv'), 'w', newline='') as f_csv:
+with open(os.path.join(save_dir, 'test.csv'), 'w', newline='') as f_csv:
     csv_output = csv.writer(f_csv)
-    csv_output.writerow(["file_name", "key", "label", "xmin", "ymin", "xmax", "ymax"])
+    csv_output.writerow(["file_name","width","height","key", "label", "xmin", "ymin", "xmax", "ymax"])
     
-    for single_file in glob.glob("./labels/*.json"):
+    for single_file in glob.glob("./annotations/*.json"):
         #print(single_file)
         change_name = os.path.basename(single_file)
         filename = change_name.split( ".", 2) [0] + ".jpg"
@@ -21,6 +45,8 @@ with open(os.path.join(dir, 'test.csv'), 'w', newline='') as f_csv:
             done = csv_output.writerow([
                 #single_file,
                 filename,
+                json_data["width"],
+                json_data["height"],
                 object["key"],
                 object["label"],
                 object["bbox"]["xmin"],
