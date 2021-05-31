951c5423b123b6eb8acc5dcaede96c601b8a2328
Laudari Sudip <53330430+Laudarisd@users.noreply.github.com>
2021-05-31 21:53:16 +0900

    Update csv_to_voc_xml.py

--- csv_to_voc_xml.py
+++ csv_to_voc_xml.py
@@ -1,64 +1,73 @@
-import pandas as pd
-import numpy as np
-from lxml import etree
-import xml.etree.ElementTree as ET
+from collections import defaultdict
+import os
 import csv
 
+from xml.etree.ElementTree import parse, Element, SubElement, ElementTree
+import xml.etree.ElementTree as ET
 
-#change these lines
-save_path = "./test/xmls"
-filename = './test/labels.csv'
+save_root2 = "test_xmls"
 
-if not os.path.exists(save_path):
-    os.mkdir(save_path)
-
-
-bbox_list = []
+if not os.path.exists(save_root2):
+    os.mkdir(save_root2)
 
 
-with open(filename, 'r') as csvfile:
-    datareader = csv.reader(csvfile)
-    for row in datareader:
-        count = 0
-        #print(row[7])
-        filename = row[0] #check file name in csv column
-        width = row[1]  #check width in csv column
-        height = row[2] #check height in csv column
-        classes = row[3] #check class in csv column
-        xmin = row[4] #check xmin in csv column
-        ymin = row[5] #check ymin in csv column
-        xmax = row[6] #check xmax in csv column
-        ymax = row[7]  #check ymax in csv column
+def write_xml(folder, filename, bbox_list):
+    root = Element('annotation')
+    SubElement(root, 'folder').text = folder
+    SubElement(root, 'filename').text = filename
+    SubElement(root, 'path').text = './images' +  filename
+    source = SubElement(root, 'source')
+    SubElement(source, 'database').text = 'Unknown'
+
+
+    # Details from first entry
+    e_filename, e_width, e_height, e_class_name, e_xmin, e_ymin, e_xmax, e_ymax = bbox_list[0]
+    
+    size = SubElement(root, 'size')
+    SubElement(size, 'width').text = e_width
+    SubElement(size, 'height').text = e_height
+    SubElement(size, 'depth').text = '3'
+
+    SubElement(root, 'segmented').text = '0'
+
+    for entry in bbox_list:
+        e_filename, e_width, e_height, e_class_name, e_xmin, e_ymin, e_xmax, e_ymax = entry
         
-        for i in range(0, 2):
-            height = df['height'].iloc[i]
-            width = df['width'].iloc[i]
-            depth = 3
-            annotation = ET.Element('annotation')
-            ET.SubElement(annotation, 'folder').text = 'images'
-            ET.SubElement(annotation, 'filename').text = filename
-            ET.SubElement(annotation, 'path').text = './test/'
-            source = ET.SubElement(annotation, 'source')
-            ET.SubElement(source, 'database').text = 'Unknown'
-            size = ET.SubElement(annotation, 'size')
-            ET.SubElement(size, 'width').text = str(width)
-            ET.SubElement(size, 'height').text = str(height)
-            ET.SubElement(size, 'depth').text = '3'
+        obj = SubElement(root, 'object')
+        SubElement(obj, 'name').text = e_class_name
+        SubElement(obj, 'pose').text = 'Unspecified'
+        SubElement(obj, 'truncated').text = '0'
+        SubElement(obj, 'difficult').text = '0'
 
-            #segmented = ET.SubElement(annotation, 'segmented')
-            ET.SubElement(annotation, 'segmented').text = '0'
-            ob = ET.SubElement(annotation, 'object')
-            ET.SubElement(ob, 'name').text = classes
-            ET.SubElement(ob, 'pose').text = 'Unspecified'
-            ET.SubElement(ob, 'truncated').text = '0'
-            ET.SubElement(ob, 'difficult').text = '0'
-            bbox = ET.SubElement(ob, 'bndbox')
-            ET.SubElement(bbox, 'xmin').text = str(xmin)
-            ET.SubElement(bbox, 'ymin').text = str(ymin)
-            ET.SubElement(bbox, 'xmax').text = str(xmax)
-            ET.SubElement(bbox, 'ymax').text = str(ymax)
+        bbox = SubElement(obj, 'bndbox')
+        SubElement(bbox, 'xmin').text = e_xmin
+        SubElement(bbox, 'ymin').text = e_ymin
+        SubElement(bbox, 'xmax').text = e_xmax
+        SubElement(bbox, 'ymax').text = e_ymax
 
-            fileName = str(df['filename'].iloc[i])
-            tree = ET.ElementTree(annotation)
-            tree.write(os.path.join(save_path, filename[:-4] + ".xml"), encoding='utf8')
-            count += 1    
+    #indent(root)
+    tree = ElementTree(root)
+    
+    xml_filename = os.path.join('.', folder, os.path.splitext(filename)[0] + '.xml')
+    tree.write(xml_filename)
+    
+
+entries_by_filename = defaultdict(list)
+
+with open('labels.csv', 'r', encoding='utf-8') as f_input_csv:
+    csv_input = csv.reader(f_input_csv)
+    header = next(csv_input)
+
+    for row in csv_input:
+        filename, width, height, class_name, xmin, ymin, xmax, ymax = row
+        entries_by_filename[filename].append(row)
+
+        #if class_name == "yes":  #for more class - if class_name in ["traffic_sign", "car", "bike"]
+        #    entries_by_filename[filename].append(row)
+        #else:
+        #    entries_by_filename[filename].append(row)
+
+
+for filename, entries in entries_by_filename.items():
+    print(filename, len(entries))
+    write_xml(save_root2, filename, entries)
